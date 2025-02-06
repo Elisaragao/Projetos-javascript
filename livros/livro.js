4eeee19253a8b6747faee7a891f43830264e2966
Elisaragao <elisangelaaragao644@gmail.com>
2025-02-06 15:05:18 -0300

    Adicionando livros em uma lista

---
+++ livros/livro.js
@@ -0,0 +1,10 @@
+let livros = []
+let livrosLista = document.getElementById("exibir")
+
+function exibir(){
+    let novoLivro = prompt("Qual o nome do livro que deseja adicionar? ")  
+    livros.push(novoLivro)
+    livrosLista.innerText = livros
+
+    
+}
