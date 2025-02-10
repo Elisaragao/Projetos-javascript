c816f77c3eab56d51f6e16476127236a71e3c070
Elisaragao <elisangelaaragao644@gmail.com>
2025-02-10 16:07:37 -0300

    Alterar as propriedades de estilo no arquivo js

---
+++ propriedadeEstilo/script.js
@@ -0,0 +1,12 @@
+
+
+function mudar() {
+    let nome = prompt("Digite um nome")
+
+    if (nome === "Elis") {
+        document.body.style.backgroundColor = "pink"
+    } else {
+        document.body.style.backgroundColor = "yellow"
+
+    }
+}
