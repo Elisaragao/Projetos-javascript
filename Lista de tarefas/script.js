b059b02fa52fae8131f28ab31fae3ab86ec7f003
Elisaragao <elisangelaaragao644@gmail.com>
2025-02-13 14:56:54 -0300

    Adicionada a função de excluir uma tarefa

--- Lista de tarefas/script.js
+++ Lista de tarefas/script.js
@@ -4,31 +4,46 @@ let buttonElement = document.querySelector("#app button")
 
 let tarefas = []
 
-function renderTarefas(){
+function renderTarefas() {
+
     listElement.innerHTML = ""
     tarefas.map((tarefa) => {
-        
-
-        let liElement= document.createElement('li')
+        let liElement = document.createElement('li')
         let tarefaText = document.createTextNode(tarefa)
 
+        let linkElement = document.createElement('a')
+        linkElement.setAttribute('href', '#')
+
+        let linkText = document.createTextNode("Excluir")
+        linkElement.appendChild(linkText)
+        
+        let posicao = tarefas.indexOf(tarefa)
+        linkElement.setAttribute("onclick", `excluirTarefa(${posicao})`)
+
         liElement.appendChild(tarefaText)
+        liElement.appendChild(linkElement)
         listElement.appendChild(liElement)
 
     })
 
 }
-function adicionarTarefa(){
-    if(inputElement.value === ''){
+
+function adicionarTarefa() {
+    if (inputElement.value === '') {
         alert("Digite alguma tarefa")
         return false
-    }else{
+    } else {
         let novaTarefa = inputElement.value
-       
+
         tarefas.push(novaTarefa)
         inputElement.value = ''
 
         renderTarefas()
     }
+}
+
+function excluirTarefa(posicao){
+alert("A posição do item é: " + posicao)
+tarefas.splice(posicao, 1)
 }
 buttonElement.onclick = adicionarTarefa
